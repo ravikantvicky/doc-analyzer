316c26b3796c88bcf5c1586ef97f5e76dd17899c
Ravi Kant Pandey <ravikantvicky@gmail.com>
2019-06-13 00:03:17 +0530

    service changes

--- rest-services/doc-analyzer/src/main/java/com/infosys/stg/doc/DocAnalyzerApplication.java
+++ rest-services/doc-analyzer/src/main/java/com/infosys/stg/doc/DocAnalyzerApplication.java
@@ -2,8 +2,12 @@ package com.infosys.stg.doc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({ @PropertySource("classpath:doc-analyzer.properties"), @PropertySource("classpath:db.properties"),
+		@PropertySource("messages.properties") })
 public class DocAnalyzerApplication {
 
 	public static void main(String[] args) {
